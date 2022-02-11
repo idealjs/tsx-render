fcd0b7f1e090fec52eabaf49c49de51591889ba6
cqh963852 <cqh963852@hotmail.com>
2022-02-11 16:06:58 +0800

    chore: remove useless code

--- packages/corn-html/src/index.ts
+++ packages/corn-html/src/index.ts
@@ -1,10 +1,1 @@
-// import App from "./App";
-// import hyper from "./lib/hyper";
-// import render from "./lib/render";
-
-// window.onload = () => {
-//     const root = document.getElementById("App");
-//     render(hyper(App, {}), root);
-// };
-
 export { default as render } from "./lib/render";
