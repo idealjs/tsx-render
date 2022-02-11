f3f4cf1b5ed09a95d3d0a4ff1a08e1256b2bcaae
cqh963852 <cqh963852@hotmail.com>
2022-02-11 11:10:00 +0800

    chore: remove fixture

--- fixtures/cron-reactive-fixture/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "@idealjs/corn";
-import App from "./App";
-
-window.onload = () => {
-    const root = document.getElementById("App");
-    createRoot(() => {
-        root?.append(App());
-    });
-};
